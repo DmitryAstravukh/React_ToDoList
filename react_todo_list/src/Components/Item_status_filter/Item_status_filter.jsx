478ff47c0851e1cba49d15bea8aae0deae2fdf99
DmitryAstravukh <astrauh1.dima1@ya.ru>
2020-06-17 15:05:56 +0300

    filter buttons refactor

--- react_todo_list/src/Components/Item_status_filter/Item_status_filter.jsx
+++ react_todo_list/src/Components/Item_status_filter/Item_status_filter.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
 export default class ItemStatusFilter extends React.Component{
+  buttons = [
+    {className:"btn btn-outline-info", label:"Все", isActive: true, onClick: ""},
+    {className:"btn btn-outline-success", label:"Завершенные", isActive: false, onClick: ""},
+    {className:"btn btn-outline-danger", label:"Важные", isActive: false, onClick: ""}
+  ];
+
   render(){
     return(
       <div className="btn-group">
-        <button type="button" className="btn btn-outline-info active">Все</button>
-        <button type="button" className="btn btn-outline-success">Завершенные</button>
-        <button type="button" className="btn btn-outline-danger">Важные</button>  
+        {
+          this.buttons.map(({className, label, isActive}) => {
+            if(isActive) className += ' active';           
+            return (<button key={label} type="button" className={className}>{label}</button>)
+          })
+        }
       </div>
     )
   }
